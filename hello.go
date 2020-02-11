20fbca19dc220681a7563f5b5b01c96010104c55
Frank Fichtenmüller <frank.fichtenmueller@sclable.com>
2020-02-11 15:39:55 +0100

    testing

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+}
+
+func whatever() {
+	fmt.Printf("why should i care?")
+}
---
+++ start.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib as mpl
+
+print('testing the file functionality')
