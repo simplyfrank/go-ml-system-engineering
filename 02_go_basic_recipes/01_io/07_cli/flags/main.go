e65bde15d2a7fa800b70dddd5a21ac109765d4f3
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2020-02-20 21:18:39 +0100

    (add) update documentation

--- 02_go_basic_recipes/01_io/07_cli/flags/main.go
+++ 02_go_basic_recipes/01_io/07_cli/flags/main.go
@@ -1,5 +1,18 @@
-package main
+// COMMAND LINE INTERFACE - Example using the flag package
+//
+//This recipe attempts to demonstrate most of the common usages of the flag package. It shows custom variable types, a
+//variety of built-in variables, shorthand flags, and writing all flags to a common structure. This is the first recipe
+//to require a main function, as the main usage of flag (flag.Parse()) should be called from main. As a result, the normal
+//example directory is omitted.
+//
+//The example usage of this application shows that you get -h automatically to get a list of flags that are included.
+//	Some other things to note are Boolean flags that are invoked without arguments, and that the flag order doesn't matter.
+//The flag package is a quick way to structure input for command-line applications and provide a flexible means of
+//specifying upfront user input for things such as setting up log levels or the verbosity of an application. In the
+//Using command-line arguments recipe, we'll explore flagsets and switch between them using arguments.
 
+
+package main
 
 import (
 	"flag"
