b908914b28a796750465a37f1580450d0fc37bf8
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2020-02-21 16:23:39 +0100

    (fix) moved server example

---
+++ 02_go_basic_recipes/01_io/07_cli/go.mod
@@ -0,0 +1,3 @@
+module github.com/skillsmart/go/01_io/07_cli
+
+go 1.13
---
+++ 02_go_basic_recipes/04_serving/httpserver/server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// To define the Handler functions to use with the http.HandlerFunc method
+// we have to accept a http.ResponseWriter and a request object
+func responseHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `
+	Host: %s
+	Methos: %s
+	RemoteAddr: %s
+	Response: %v
+	URL: %s
+`, r.Host, r.Method, r.RemoteAddr, r.Response, r.URL)
+}
+
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `
+	<h1>Welcome to the show</h1>
+	<p>This page is a welcome page for you alone {{.}}</p>
+`)
+}
+
+func main() {
+	// We are defining the routes we want to publish
+	// Each route gets assigned a handler function
+	http.HandleFunc("/response/", responseHandler )
+	http.HandleFunc("/:name/", nameHandler)
+
+	// WE can use the shorthand way to set a server
+	//log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// Or create a custom server value
+	s := &http.Server{
+		Addr:              ":8080",
+		Handler:           nil,
+		TLSConfig:         nil,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 0,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       0,
+		MaxHeaderBytes:    1 << 20,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+	// Now we are calling the Listen and Serve loop directly on our
+	// custom server instance
+	log.Fatal(s.ListenAndServe())
+}
