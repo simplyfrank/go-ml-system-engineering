52b5788bd27b3d7ad3cdb1e8bcd8d201c4807e0e
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2020-02-21 18:57:49 +0100

    (add) Page generation using templates

---
+++ 02_go_basic_recipes/04_serving/using_templates/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+var tpl *template.Template
+var pd pageData
+var err error
+
+type pageData struct {
+	Title 		string
+	CompanyName string
+}
+
+func main() {
+	// This executes on the start of the package before main
+	// and allos us to set package scoped variables
+	tpl, err = template.ParseGlob("./templates/*")
+	if err != nil { log.Println("Unable to load templates: ", err); os.Exit(-1)}
+
+	pd = pageData{
+		Title:       "SkillSmart Evolutions",
+		CompanyName: "ABetterStartup Ltd",
+	}
+
+	// Set the handler functions
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	// Start the server
+	http.ListenAndServe(":8080", nil)
+}
+
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	// Handle the presentation of the homepage
+	w.WriteHeader(200)
+	fmt.Println("Generating Home page")
+	tpl.ExecuteTemplate(w, "index.gohtml", pd)
+}
+
+func aboutHandler(w http.ResponseWriter, req *http.Request) {
+	// Handle the presentation fo the about page
+	w.WriteHeader(200)
+	fmt.Println("Generating About page")
+	tpl.ExecuteTemplate(w, "about.gohtml", pd)
+}
