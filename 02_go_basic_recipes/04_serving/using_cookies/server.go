e2de0f788e9ad1045cfa4aa6196b94f5f41bfd4b
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2020-02-21 19:31:24 +0100

    (add) Read and write Cookies with net/http

---
+++ 02_go_basic_recipes/04_serving/using_cookies/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", setHandler)
+	http.HandleFunc("/read", readHandler)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func setHandler(w http.ResponseWriter, req *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name: "my-cookie",
+		Value: "some value set",
+	})
+	fmt.Fprintf(w, "Cookie written. Check your browsers cache")
+	fmt.Fprintln(w, "In Chrome go to dev tools / application / cookies")
+}
+
+func readHandler(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("my-cookie")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	fmt.Fprintln(w, "Your Cookie: ", c)
+
+}
